334f5983afa9c8daaeb76e2af4a7ab7e6484de19
Olivia Hao <olivha@amazon.com>
2024-01-28 20:57:21 -0500

    updated the model

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import cv2
 import requests
 from io import BytesIO
 from flask import Flask, request
+import pandas as pd
+import math
 
 # Load the YOLO model
 model = YOLO('./yolov8n.pt')
@@ -23,10 +25,21 @@ def predict_image():
     image = Image.open(BytesIO(response.content))
 
     # Make a prediction
-    prediction = model(image)
+    predictions = model(image, save_conf=True, conf = 0.2)
+    results = []
+    class_id = model.names
+    for prediction in predictions:
+        boxes = prediction.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls)
+            results.append([x1,y1,x2,y2, confidence, class_id[int(cls)]])
 
-    # Return the prediction
-    return str(prediction[0])
+    df = pd.DataFrame(results, columns=['x1', 'y1', 'x2', 'y2', 'Confidence', 'Class ID'])
+
+    return df.to_json(orient='records')
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5000)
